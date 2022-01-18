b24023fc47aae65153ea2d3d89d7640c6c483bb5
Ken_11 <73392970+KenLegend11@users.noreply.github.com>
2022-01-17 21:36:30 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 new fullpage("#fullpage", {
+    navigation: true,
+    autoScrollin: true,
+    menu: '#main-header',
+    anchors: ['Home', 'What_is', 'What_could_cause', 'How_is_treated', 'Contact'],
 
-    navigation: true
+
 })
